d1c7debfb280ec0041181988889bf3f06e9bd4eb
Andrew Maney <maneyandrew@gmail.com>
2021-08-08 13:24:07 -0600

    starting refactor to allow for onlyModifiedFiles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
 const Client = require('ssh2-sftp-client')
 
 const host = core.getInput('host')
@@ -7,12 +8,21 @@ const username = core.getInput('username')
 const password = core.getInput('password')
 const sourceDir = core.getInput('sourceDir')
 const targetDir = core.getInput('targetDir')
-
-core.info(`connecting to ${username}@${host}:${port}...`)
+const onlyModifiedFiles = core.getInput('onlyModifiedFiles')
 
 let sftp = new Client()
-sftp
-  .connect({
+
+async function run() {
+  if (onlyModifiedFiles) {
+    core.info('getting modified files...')
+    const myToken = core.getInput('myToken')
+    const myTolkien = github.context.token
+    core.info(`tokens: ${myToken} ${myTolkien}`)
+  }
+
+  core.info(`connecting to ${username}@${host}:${port}...`)
+
+  await sftp.connect({
     host,
     port,
     username,
@@ -20,17 +30,19 @@ sftp
     readyTimeout: 5000,
     retries: 5,
   })
-  .then(() => {
-    core.info(`connected \n uploading ${sourceDir} to ${targetDir}...`)
-    return sftp.uploadDir(sourceDir, targetDir)
-  })
-  .then(() => {
-    core.info(`succesfully uploaded ${sourceDir} to ${targetDir} 🎉`)
-  })
-  .catch((error) => {
-    core.setFailed(error.message)
-  })
-  .finally(() => {
-    core.info('ending SFTP session')
-    sftp.end()
-  })
+
+  core.info(`connected \n uploading ${sourceDir} to ${targetDir}...`)
+
+  await sftp.uploadDir(sourceDir, targetDir)
+
+  core.info(`succesfully uploaded ${sourceDir} to ${targetDir} 🎉`)
+}
+
+try {
+  run()
+} catch (error) {
+  core.setFailed(error.message)
+} finally {
+  core.info('ending SFTP session')
+  sftp.end()
+}
